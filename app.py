c738b93e757063afa7963b3876223b7b3500d0d1
DollyPy <ddolapo1@gmail.com>
2022-10-07 18:56:18 +0100

    process

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import logging
 from flask import Flask, render_template, Response, request, jsonify
 from flask_socketio import SocketIO
 from camera import Camera
+import cv2
 from utils import base64_to_pil_image, pil_image_to_base64
 # import jsonify
 
-
+global camera
 #----------------- Video Transmission ------------------------------#
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(stdout))
@@ -43,6 +44,14 @@ def gen():
     app.logger.info("starting to generate frames!")
     while True:
         frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
+        detector=cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
+        faces=detector.detectMultiScale(frame,1.2,6)
+         #Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -52,6 +61,14 @@ def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/start',methods=['POST'])
+def start():
+    return render_template('index.html')
 
+@app.route('/stop',methods=['POST'])
+def stop():
+    if camera.isOpened():
+        camera.release()
+    return render_template('stop.html')
 if __name__ == '__main__':
     socketio.run(app)
